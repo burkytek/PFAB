c140a0c3a0ed130a783205286546e3e4d9850029
burkytek <vojtaburkert@seznam.cz>
2014-07-21 19:48:30 +0200

    Homework created

---
+++ homework.py
@@ -0,0 +1,38 @@
+first = "Vojta"
+last = "Burkert"
+age = 23
+
+print first,last,age
+
+
+
+import math
+sideA = 12.55
+
+sideB = 17.85
+
+sideCSquared = sideA*sideA + sideB*sideB
+
+sideC = math.sqrt(sideCSquared)
+
+print sideC
+
+
+
+operand1 = 95
+
+operand2 = 64.5
+
+
+print operand1,"+",operand2,"=",operand1+operand2
+
+print operand1,"-",operand2,"=",operand1-operand2
+
+print operand1,"*",operand2,"=",operand1*operand2
+
+print operand1,"/",operand2,"=",operand1/operand2
+
+print operand1,"%",operand2,"=",operand1%operand2
+
+
+# My solution is the best, because after change in operand I don't need to improve the whole code.
